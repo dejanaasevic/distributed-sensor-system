2ea93a6d079fe8588abddffd343b40609425413d
iigor000 <igor.novakovic2004@gmail.com>
2026-06-09 20:28:38 +0200

    Added notification hub

---
+++ src/NotificationService/NotificationHub.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace NotificationService
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendNotification(string message)
+        {
+            Console.WriteLine($"[NotificationHub] Sending notification: {message}");
+            await Clients.All.SendAsync("ReceiveNotification", message);
+        }
+    }
+}
