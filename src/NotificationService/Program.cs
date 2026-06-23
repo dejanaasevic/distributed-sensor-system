49f5ab130d6857262a88ae8683e7d7becd6b0a01
iigor000 <igor.novakovic2004@gmail.com>
2026-06-24 01:24:32 +0200

    Fixed kubenetes setup

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -5,7 +5,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
 builder.Services.AddHealthChecks();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyMethod()
+                   .AllowAnyHeader()
+                   .SetIsOriginAllowed(_ => true) // Critical for external IPs
+                   .AllowCredentials(); // Required for SignalR
+        });
+});
+
 var app = builder.Build();
+
+
+
+app.UseCors("AllowAll");
 
 app.MapHub<NotificationHub>("/notificationHub");
 app.MapHealthChecks("/health");
